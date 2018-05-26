4104344ab8dbaaaa6dc280644c42bf335465fd74
Svetoslav Popov <svetoslavpopov93@gmail.com>
2018-05-26 14:52:10 +0200

    Watch added

---
+++ ios/allInWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  allInWatch Extension
+//
+//  Created by Svetoslav Popov on 26.05.18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
